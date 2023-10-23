a6a2de3a4b799829e7d28cadec3fcaa8f1ba34b0
Constantine Havansky <be.a.reety@gmail.com>
2023-10-23 20:04:01 +0200

    Add second activity view

--- app/src/main/java/com/khavanski/caffeapp/MakeOrderActivity.kt
+++ app/src/main/java/com/khavanski/caffeapp/MakeOrderActivity.kt
@@ -1,15 +1,11 @@
 package com.khavanski.caffeapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MakeOrderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_make_order)
-        val intent = Intent(this, MakeOrderActivity::class.java)
-        startActivity(intent)
-
     }
 }
