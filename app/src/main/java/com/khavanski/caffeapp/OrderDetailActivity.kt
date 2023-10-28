dfe05042aad8601ae9b72e812475a33eeec6247e
Constantine Havansky <be.a.reety@gmail.com>
2023-10-28 12:57:39 +0200

    Finishing screens

--- app/src/main/java/com/khavanski/caffeapp/OrderDetailActivity.kt
+++ app/src/main/java/com/khavanski/caffeapp/OrderDetailActivity.kt
@@ -1,15 +1,11 @@
 package com.khavanski.caffeapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class OrderDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order_detail)
-        val intent = Intent(this, OrderDetailActivity::class.java)
-        startActivity(intent)
-
     }
 }
