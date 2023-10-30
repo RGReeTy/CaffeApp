5e20170432bc794243667fabbec2f3e7629d398d
Constantine Havansky <be.a.reety@gmail.com>
2023-10-30 13:43:17 +0100

    Add logic for first screen

--- app/src/main/java/com/khavanski/caffeapp/MainActivity.kt
+++ app/src/main/java/com/khavanski/caffeapp/MainActivity.kt
@@ -1,15 +1,45 @@
 package com.khavanski.caffeapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editTextName: EditText
+    private lateinit var editTextPassword: EditText
+    private lateinit var buttonSignIn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_order_detail)
-        val intent = Intent(this, OrderDetailActivity::class.java)
-        startActivity(intent)
+        setContentView(R.layout.activity_main)
 
+        initViews()
+
+        buttonSignIn.setOnClickListener {
+            val username = editTextName.text.toString().trim()
+            val password = editTextPassword.text.toString().trim()
+
+            if (username.isBlank() || password.isBlank()) {
+                Toast.makeText(this, R.string.errorFieldsEmpty, Toast.LENGTH_SHORT).show()
+            } else {
+                launchNextScreen(username)
+            }
+        }
+
+    }
+
+    private fun launchNextScreen(username: String) {
+        val intent = MakeOrderActivity.newIntent(this, username)
+        startActivity(intent)
+    }
+
+    private fun initViews() {
+        editTextName = findViewById(R.id.editTextName)
+        editTextPassword = findViewById(R.id.editTextPass)
+        buttonSignIn = findViewById(R.id.signInButton)
     }
 }
--- app/src/main/java/com/khavanski/caffeapp/MakeOrderActivity.kt
+++ app/src/main/java/com/khavanski/caffeapp/MakeOrderActivity.kt
@@ -1,11 +1,21 @@
 package com.khavanski.caffeapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MakeOrderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_make_order)
+    }
+
+    companion object {
+        fun newIntent(context: Context, username: String): Intent {
+            val intent = Intent(context, MakeOrderActivity::class.java)
+            intent.putExtra("username", username)
+            return intent
+        }
     }
 }
